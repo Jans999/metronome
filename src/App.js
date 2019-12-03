ee810d0acda406c86d16218bb35ee1f73b20cee2
Jannick Moth <jannickmoth@hotmail.co.uk>
2019-12-03 18:55:40 +0000

    creating the frame

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+     
+      <Header />
+
+
+      <main>
+
+        <div className="button">
+            <button>Play</button>
+        </div>
+
+        <div className="slider">
+            <label for="bpm"> 150 BPM</label>
+            <input type="range" min="45" max="150" name="bpm"></input>
+        </div>
+
+      </main>
     </div>
   );
 }
